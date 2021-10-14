7e2a38d988f698183fc0e054a7308c9ca1a69505
Daviiduhh <daviiduhhs@gmail.com>
2021-10-14 13:40:28 -0500

    Primera version de perritosVueAPI

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="fluid-container bg-dark">
+    <div class="container p-5 text-center">
+      <h1 class="text-center text-light pb-5">Dog API</h1>
+      <img :src="dogImg[counter]" alt="dog" class="img-fluid rounded d-block mx-auto dogImg"/>
+      <button v-on:click="counter += 1" class="btn btn-light btn-lg m-5">Ver más perritos</button>
+      <p class="text-light">Has visto {{ counter }} perritos</p>
+    </div>
+  </div>
+
+  <footer class="fluid-container bg-dark p-5">
+    <p class="text-light text-center">Powered by
+      <a href="https://daviiduhh.com" class="daviiduhh">daviiduhh</a>
+    </p>
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      dogImg: [],
+      counter: 0
+    };
+  },
+  async mounted() {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random/50");
+    const data = await res.json();
+
+    this.dogImg = data.message;
+
+    this.counter += 1
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .dogImg {
+    max-width: 100%;
+    max-height: 80vh;
+  }
+
+  .daviiduhh {
+    text-decoration: none;
+    color: white;
+  }
+
+  .daviiduhh:hover {
+    color: aquamarine;
+  }
 </style>
